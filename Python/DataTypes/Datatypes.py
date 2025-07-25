7c95f0e34351f4094429af8ee0ad3f4c93bef690
Veeresh-Hanni <veereshhanni347@gmail.com>
2025-07-25 11:37:28 +0530

    Upload datatypes in Python

---
+++ Python/DataTypes/Datatypes.py
@@ -0,0 +1,41 @@
+# Demonstrating basic data types in Python
+
+# Integer
+num = 10
+
+# Float
+price = 19.99
+
+# String
+name = "Alice"
+
+# Large integer
+large_num = 12345678901234567890
+
+# long float
+long_float = 3.14159265358979323846
+
+# Boolean
+is_active = True
+
+# List
+fruits = ["apple", "banana", "cherry"]
+
+# Tuple
+coordinates = (10.0, 20.0)
+
+# Dictionary
+person = {"name": "Bob", "age": 25}
+
+# Setgit add
+unique_numbers = {1, 2, 3, 4}
+
+# Printing types
+print(type(num))  # <class 'int'>
+print(type(price))  # <class 'float'>
+print(type(name))  # <class 'str'>
+print(type(is_active))  # <class 'bool'>
+print(type(fruits))  # <class 'list'>
+print(type(coordinates))  # <class 'tuple'>
+print(type(person))  # <class 'dict'>
+print(type(unique_numbers))  # <class 'set'>
