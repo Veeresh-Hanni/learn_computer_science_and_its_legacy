0854ea4ecdbe440897e9738a7a91f1a664631a76
Veeresh-Hanni <veereshhanni347@gmail.com>
2025-07-25 11:38:05 +0530

    Upload Functions in Python

---
+++ Python/Functions/Functions2.py
@@ -0,0 +1,15 @@
+# Function declarartion and definition
+def enoMadabeda():
+    # do nothing
+    pass
+
+
+def print_message() -> None:  # Function definition
+    # Indentation is super importanat in Python
+    # It is like curly braces in other language
+    print("Namaskara Bharath!")
+
+
+# Function invocation / calling
+enoMadabeda()
+print_message()
