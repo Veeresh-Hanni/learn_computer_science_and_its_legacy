f4c0d94a80b32cf7c23cd93c70298d84dc23cfa9
Veeresh-Hanni <veereshhanni347@gmail.com>
2025-08-08 11:55:21 +0530

    Working with modules and packes in Python

---
+++ Python/Packages_modules/my_project/DataStructures/mylist.py
@@ -0,0 +1,17 @@
+class MyList:
+
+    def __init__(self):
+        self.list = []
+
+    def addElements(self, item):
+        self.list.append(item)
+
+    def printList(self):
+        print(self.list)
+
+    def removeElemnt(self, item):
+        self.list.remove(item)
+
+
+class StudentList(MyList):
+    pass
---
+++ Python/Packages_modules/my_project/application.py
@@ -0,0 +1,18 @@
+from DataStructures.mylist import MyList
+from analysis.myanalysis import MyAnalysis
+
+
+new_list = MyList()
+
+new_list.addElements(23)
+new_list.addElements(2)
+new_list.addElements(25)
+new_list.addElements(27)
+
+new_list.removeElemnt(2)
+new_list.printList()
+
+
+new_analysis = MyAnalysis()
+new_analysis.list.append(2)
+print(new_analysis.list)
