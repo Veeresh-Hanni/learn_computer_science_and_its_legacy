8343c8a16de770d9309524616441d5f8e4aaa8fa
Veeresh-Hanni <veereshhanni347@gmail.com>
2025-08-14 12:34:40 +0530

    Singly liked list search key and prinat all nodes insidelist function

--- Python/DataStructures/LinkedList/SinglyLinkedList/singly_linkedlist.py
+++ Python/DataStructures/LinkedList/SinglyLinkedList/singly_linkedlist.py
@@ -36,8 +36,27 @@ class SinglyLinkedList:
         if self.head is None:
             return
         
+
+        # two or more nodes present
         self.head = self.head.next
 
+    def delete_at_end(self):
+        # list is empty
+        if self.head == None:
+            return
+        
+        # list has one node
+        if self.head.next == None:
+            self.head = None
+            return
+
+        # two or more nodes in list
+        current_node = self.head
+
+        while(current_node.next.next != None):
+            current_node = current_node.next
+
+        current_node.next = None
 
     def insert_at_given_position(self, data, insert_position):
         # position invalid
@@ -104,9 +123,44 @@ class SinglyLinkedList:
         
         current_node.next = current_node.next.next
 
+    def print_nodes(self):
+        # if list is empty
+        if self.head == None:
+            print("List is empty")
+            return
+        
+        # not list empty
+        current_node = self.head
 
+        while current_node is not None:
+
+            print(current_node.data, "->", end=" ")
+
+            current_node = current_node.next
+
+        print("None")
+
+    def search(self, key):
+        # if list is empty
+        if self.head == None:
+            print("List is empty")
+            return
+        
+        # not list empty
+        current_node = self.head
+        while current_node is not None:
+
+            if (key == current_node.data):
+                
+                print(f"{key} Found")
+                return
+            
+            current_node = current_node.next
+
+        print("not found")
 # invocation code for insert/delete at_given_position 
 def insert_delete_at_given_position(list: SinglyLinkedList):
+
 
     list.insert_at_given_position(data = 10, insert_position = -1)
     list.delete_at_given_position(delete_position = -1)
@@ -120,22 +174,61 @@ def insert_delete_at_given_position(list: SinglyLinkedList):
     list.insert_at_given_position(data = 100, insert_position = 10)
 
     # list.delete_at_given_position(1)
+
+# invocation code for delete at end beginning
+def delete_opearations_start_end(list: SinglyLinkedList):
+    list.delete_at_beginning()
+    list.delete_at_end()
+
+    list.insert_at_beginning(12)
+    list.delete_at_beginning()
+
+    list.insert_at_beginning(10)
+    list.delete_at_end()
+
+    list.insert_at_beginning(13)
+    list.insert_at_beginning(24)
+    list.insert_at_beginning(28)
+    list.delete_at_end()
+    list.delete_at_end()
+
+def print_list_driver_code(list: SinglyLinkedList):
+    list.print_nodes()
+    list.insert_at_beginning(13)
+    list.print_nodes()
+
+    # list.insert_at_beginning(28)
+
+    list.insert_at_given_position(23,2)
+    list.print_nodes()
+
+    list.insert_at_beginning(55)
+    list.print_nodes()
+
+def search_driver_code(list: SinglyLinkedList):
+
+    list.search(23)
+    list.search(5)
 # driver code to test above class
 if __name__ == "__main__":
     # create Singly Linked list
 
-    sl = SinglyLinkedList()
+    list = SinglyLinkedList()
 
     # insert a element
-    # sl.insert_at_beginning(1)
-    sl.insert_at_beginning(34)
+    # list.insert_at_beginning(1)
+    # slist.insert_at_beginning(34)
 
     # inserrt at given position
-    insert_delete_at_given_position(sl)
+    # insert_delete_at_given_position(list)
 
-    print("List is Created successfully")
+    # print("List is Created successfully")
 
-    # print data
-    print("Head Node data is ", sl.head.data)
-    print("Next node data is", sl.head.next.data)
-    # print("Next node data is", sl.head.next.next.data)
+    # # print data
+    # print("Head Node data is ", list.head.data)
+    # print("Next node data is", list.head.next.data)
+    # print("Next node data is", list.head.next.next.data)
+
+    # delete_opearations_start_end(list)
+    print_list_driver_code(list)
+    search_driver_code(list)
